b1bc3209ae6ae095c9b6408e45768d2fe1613e22
ander-ortiz <ortizone620@gmail.com>
2025-06-24 13:50:19 -0600

    Add objects.js and objects.ts for object handling examples

---
+++ objetos/objects.js
@@ -0,0 +1,15 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Súper velocidad', 'Viajar en el tiempo']
+    };
+    // no se puede agregar mas cosas despues de la definición del objeto
+    flash;
+    {
+        name: 'Clark Kent';
+        age: 60;
+        powers: ['Súper fuerza'];
+    }
+})();
