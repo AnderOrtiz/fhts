49c1bd1cd9fa25751c4671ac385bcbeca0ca9b63
ander-ortiz <ortizone620@gmail.com>
2025-06-26 17:15:04 -0600

    Variables Let

---
+++ ES6/let-var.ts
@@ -0,0 +1,13 @@
+(() => {
+    // console.log('let');
+
+    const nombre = 'Ander'
+
+    const getName = () =>{
+        console.log('viejo Getname')
+    }
+
+    // getName = () => {console.log('Nuevo getName')};
+    // getName();
+
+})();
