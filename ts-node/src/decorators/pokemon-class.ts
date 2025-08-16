206a398abc71f4ead1e34dfa69c09fc7ae9b00b3
ander-ortiz <ortizone620@gmail.com>
2025-08-16 17:02:12 -0600

    Decoradores en métodos

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -16,13 +16,34 @@ const bloquearPrototipo = function(constructor: Function) {
     Object.seal(constructor.prototype)
 }
 
+function CheckValidPokemonId():Function {
+    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor){
+        // console.log({target, propertyKey, descriptor})
+
+        const originalMethod = descriptor.value;
+
+        descriptor.value = (id:number) => {
+            if (id < 1 || id > 800) {
+                return console.error('El id del pokemomn debe estar entre 1 y 800')
+            } else {
+                return originalMethod(id)
+            }
+        }
+    }
+}
 
 @bloquearPrototipo
-@printToConsoleConditional(true)
+@printToConsoleConditional(false)
 export class Pokemon {
     public publicApi: string = 'https://pokeapi.co'
 
     constructor (
         public name: string
     ){}
+
+    @CheckValidPokemonId()
+    savePokemomToDB(id:number){
+        console.log(`Pokemon guardado en DB con id ${id}`);
+    }
+
 }
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -3,6 +3,8 @@ import { Pokemon } from "./decorators/pokemon-class";
 
 const charmander = new Pokemon('Charmander');
 
-(Pokemon.prototype as any).customName = 'Pikachu'
+// (Pokemon.prototype as any).customName = 'Pikachu'
+
+charmander.savePokemomToDB(50);
 
 console.log(charmander);
